0012ff68c41eba0b9972924a3c55769501fabc30
GouravKesarkar <gourav.anand.kesarkar@gmail.com>
2025-09-14 19:46:58 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,38 @@
+const CACHE_NAME = "mythology-cache-v2"; // versioned cache
+const urlsToCache = [
+  "index.html",
+  "story.html",
+  "language.html",
+  "index_story.html",
+  "styles.css",
+  "manifest.json",
+  "stories.json",
+  "icon.png"
+];
+
+// Install
 self.addEventListener("install", event => {
-    event.waitUntil(
-      caches.open("mythology-cache").then(cache => {
-        return cache.addAll([
-          "index.html",
-          "story.html",
-          "styles.css",
-          "app.js",
-          "manifest.json",
-          "stories/stories_en.json",
-          "stories/stories_hi.json",
-          "stories/stories_mr.json"
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener("fetch", event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Activate: remove old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
